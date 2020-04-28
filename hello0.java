631f787f61bb692920eb6758992c461ec977a1e6
happy296 <64205001+happy296@users.noreply.github.com>
2020-04-28 09:07:36 +0800

    Add files via upload
    这是一个主干代码

---
+++ hello0.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.Arrays;
+
+public class hello0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] number = {4,2,9,6,8};
+	       System.out.println(Arrays.toString(number));
+	       System.out.println("1、求和");
+	       System.out.println("2、求乘积");
+	       System.out.println("3、找出最大的数");
+	       System.out.println("4、找出最小的数");
+	       System.out.println("5、从小到大排序");
+
+	}
+
+}
